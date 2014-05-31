646e555cb203ea242493b8b4a505b80d246917ea
Owain Williams <git@pilipalasoftware.co.uk>
2014-05-31 17:15:09 +0100

    Add unit test project for Pilipala.Data.

---
+++ Pilipala.Data.UnitTests/DBaseDataReaderTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Pilipala.Data.UnitTests
+{
+    [TestFixture]
+    public class DBaseDataReaderTests
+    {
+        [Test]
+        public void CanCreateDBaseDataReader()
+        {
+        }
+    }
+}
