7ab1332108284bf713d1f056db86d5d46036489e
Owain Williams <git@pilipalasoftware.co.uk>
2014-06-01 20:03:06 +0100

    Parse field name.

---
+++ Pilipala.Data.UnitTests/DBase/FieldTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Pilipala.Data.DBase;
+
+namespace Pilipala.Data.UnitTests.DBase
+{
+    [TestFixture]
+    public class FieldTests
+    {
+        [Test]
+        public void CanParseCharacterField()
+        {
+            var data = new byte[]
+                       {
+                           // Field name
+                           (byte)'C',
+                           (byte)'h',
+                           (byte)'a',
+                           (byte)'r',
+                           (byte)' ',
+                           (byte)'F',
+                           (byte)'i',
+                           (byte)'e',
+                           (byte)'l',
+                           (byte)'d',
+                           0,
+                           (byte)'C', // Field type
+                           0, 0, 0, 0, // Reserved
+                           50, // Field length
+                           0, // Field decimal count
+                           0, 0, // Reserved
+                           0, // Work area ID
+                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Reserved
+                           0 // Production MDX field flag
+                       };
+            var field = Field.Parse(data);
+
+            Assert.That(field.Name, Is.EqualTo("Char Field"));
+        }
+
+        [Test]
+        public void WillGetAnExceptionIfNotEnoughBytesHaveBeenProvided()
+        {
+            Assert.Throws<InvalidOperationException>(() => Field.Parse(new byte[] { 0, 1, 2, 3, 4, 5 }));
+        }
+    }
+}
---
+++ Pilipala.Data/DBase/Field.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+using Pilipala.Data.Extensions;
+using Pilipala.Data.Resources;
+
+namespace Pilipala.Data.DBase
+{
+    internal class Field
+    {
+        private Field(byte[] buffer)
+        {
+            var name = buffer.Take<byte>(11, 0);
+            Name = Encoding.ASCII.GetString(name, 0, name.Length);
+        }
+
+        public string Name { get; private set; }
+
+        internal static Field Parse(byte[] buffer)
+        {
+            if (buffer.Length != 32)
+            {
+                throw new InvalidOperationException(ErrorMessages.DBaseDataReader_InvalidFormat);
+            }
+
+            return new Field(buffer);
+        }
+    }
+}
