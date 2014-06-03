f3b6e2f79b7f4945b7f085ee5ff1a052726c0429
Owain Williams <git@pilipalasoftware.co.uk>
2014-06-03 14:26:36 +0100

    Add tests for resource files.

---
+++ Pilipala.Data.UnitTests/ResourceTests/ErrorMessagesResourceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+using NUnit.Framework;
+
+using Pilipala.Data.Resources;
+
+namespace Pilipala.Data.UnitTests.ResourceTests
+{
+    [TestFixture]
+    public class ErrorMessagesResourceTests
+    {
+        [Test]
+        public void CanInstantiateTheErrorMessagesResource()
+        {
+            var resource = new ErrorMessages();
+            Assert.That(resource, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanSetTheCultureOfTheErrorMessagesResource()
+        {
+            Assert.That(ErrorMessages.Culture, Is.Null);
+
+            var culture = new CultureInfo("en-GB");
+            ErrorMessages.Culture = culture;
+
+            Assert.That(ErrorMessages.Culture, Is.EqualTo(culture));
+
+            ErrorMessages.Culture = null;
+        }
+
+        [Test]
+        public void ErrorMessagesResourceDefaultCultureIsNull()
+        {
+            Assert.That(ErrorMessages.Culture, Is.Null);
+        }
+    }
+}
---
+++ Pilipala.Data.UnitTests/ResourceTests/UnitTestsResourceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+using NUnit.Framework;
+
+namespace Pilipala.Data.UnitTests.ResourceTests
+{
+    [TestFixture]
+    public class UnitTestsResourceTests
+    {
+        [Test]
+        public void CanInstantiateTheResource()
+        {
+            var resource = new Properties.Resources();
+            Assert.That(resource, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanSetTheCultureOfTheErrorMessagesResource()
+        {
+            Assert.That(Properties.Resources.Culture, Is.Null);
+
+            var culture = new CultureInfo("en-GB");
+            Properties.Resources.Culture = culture;
+
+            Assert.That(Properties.Resources.Culture, Is.EqualTo(culture));
+
+            Properties.Resources.Culture = null;
+        }
+
+        [Test]
+        public void ErrorMessagesResourceDefaultCultureIsNull()
+        {
+            Assert.That(Properties.Resources.Culture, Is.Null);
+        }
+    }
+}
