4c364d83934a126982c4981ad73e85d40352b545
Owain Williams <git@pilipalasoftware.co.uk>
2014-10-14 23:49:19 +0100

    Add Pilipala.Data and associated unit test project

---
+++ Pilipala.Data.UnitTests/Xbase/XbaseDataReaderTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Common;
+
+using FluentAssertions;
+
+using Pilipala.Data.Xbase;
+
+using Xunit;
+
+namespace Pilipala.Data.UnitTests.Xbase
+{
+    public class XbaseDataReaderTests
+    {
+        [Fact]
+        public void XbaseDataReaderDerivesFromDbDataReader()
+        {
+            typeof(XbaseDataReader).BaseType.Should().Be<DbDataReader>();
+        }
+    }
+}
