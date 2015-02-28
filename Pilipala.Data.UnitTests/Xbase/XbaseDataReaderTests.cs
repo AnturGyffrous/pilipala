bcf5a00342af6947d1bdcdb0213e4c9f932c0acf
Owain Williams <git@pilipalasoftware.co.uk>
2015-02-28 15:33:45 +0000

    Use ctor, not AutoFixture for creating objects.

--- Pilipala.Data.UnitTests/Xbase/XbaseDataReaderTests.cs
+++ Pilipala.Data.UnitTests/Xbase/XbaseDataReaderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 using System.Threading.Tasks;
 
 using FluentAssertions;
@@ -6,60 +7,62 @@ using FluentAssertions;
 using NSubstitute;
 
 using Pilipala.Data.Xbase;
-using Pilipala.Tests.AutoFixture;
 using Pilipala.Tests.Extensions;
 
-using Ploeh.AutoFixture.Xunit;
-
 using Xunit;
-using Xunit.Extensions;
 
 namespace Pilipala.Data.UnitTests.Xbase
 {
-    public class XbaseDataReaderTests
+    public class XbaseDatareaderTests
     {
         private const string _noDataHasBeenReadExceptionMessage =
             "No data has been read. You must advance the cursor past the beginning of the file by calling Read() or ReadAsync() before inspecting the data.";
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void CloseShouldBeCalledWhenDisposing([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
-        {
-            // Act
-            reader.Dispose();
+        private readonly IXbaseDataParser _parser;
 
-            // Assert
-            parser.Received().Close();
+        private readonly DbDataReader _reader;
+
+        public XbaseDatareaderTests()
+        {
+            _parser = Substitute.For<IXbaseDataParser>();
+            _reader = new XbaseDataReader(_parser);
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void DepthShouldBeZeroAfterRead([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void CloseShouldBeCalledWhenDisposing()
         {
             // Act
-            reader.Read();
+            _reader.Dispose();
 
             // Assert
-            reader.Depth.Should().Be(0);
+            _parser.Received().Close();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public async void DepthShouldBeZeroAfterReadAsync([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void DepthShouldBeZeroAfterRead()
         {
             // Act
-            await reader.ReadAsync();
+            _reader.Read();
 
             // Assert
-            reader.Depth.Should().Be(0);
+            _reader.Depth.Should().Be(0);
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void DepthShouldThrowInvalidOperationExceptionIfNoDataHasBeenRead([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public async void DepthShouldBeZeroAfterReadAsync()
         {
             // Act
-            Action depth = () => reader.Depth.IgnoreUnusedVariable();
+            await _reader.ReadAsync();
+
+            // Assert
+            _reader.Depth.Should().Be(0);
+        }
+
+        [Fact]
+        public void DepthShouldThrowInvalidOperationExceptionIfNoDataHasBeenRead()
+        {
+            // Act
+            Action depth = () => _reader.Depth.IgnoreUnusedVariable();
 
             // Assert
             depth
@@ -67,173 +70,155 @@ namespace Pilipala.Data.UnitTests.Xbase
                 .WithMessage(_noDataHasBeenReadExceptionMessage);
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void IsClosedShouldBeFalseBeforeReaderIsClosed([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void IsClosedShouldBeFalseBeforereaderIsClosed()
         {
             // Assert
-            reader.IsClosed.Should().BeFalse();
+            _reader.IsClosed.Should().BeFalse();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void IsClosedShouldBeTrueAfterReaderIsClosed([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void IsClosedShouldBeTrueAfterreaderIsClosed()
         {
             // Act
-            reader.Close();
+            _reader.Close();
 
             // Assert
-            reader.IsClosed.Should().BeTrue();
+            _reader.IsClosed.Should().BeTrue();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void NextResultAsyncShouldThrowNotImplementedException([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void NextResultAsyncShouldThrowNotImplementedException()
         {
             // Act
-            Func<Task<bool>> nextResultAsync = reader.NextResultAsync;
+            Func<Task<bool>> nextResultAsync = _reader.NextResultAsync;
 
             // Assert
             nextResultAsync.ShouldThrow<NotImplementedException>();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void NextResultShouldThrowNotImplementedException([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void NextResultShouldThrowNotImplementedException()
         {
             // Act
-            Action nextResult = () => reader.NextResult();
+            Action nextResult = () => _reader.NextResult();
 
             // Assert
             nextResult.ShouldThrow<NotImplementedException>();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public async void ReadAsyncShouldReturnFalseWhenThereAreNoMoreRecordsToRead([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public async void ReadAsyncShouldReturnFalseWhenThereAreNoMoreRecordsToRead()
         {
             // Arrange
-            parser.ReadAsync().Returns(Task.FromResult(true), Task.FromResult(true), Task.FromResult(false));
+            _parser.ReadAsync().Returns(Task.FromResult(true), Task.FromResult(true), Task.FromResult(false));
 
             // Act
-            var firstRead = await reader.ReadAsync();
-            var secondRead = await reader.ReadAsync();
-            var thirdRead = await reader.ReadAsync();
+            var firstRead = await _reader.ReadAsync();
+            var secondRead = await _reader.ReadAsync();
+            var thirdRead = await _reader.ReadAsync();
 
             // Assert
             firstRead.Should().BeTrue();
             secondRead.Should().BeTrue();
             thirdRead.Should().BeFalse();
-            parser.Received(3).ReadAsync().IgnoreAwaitWarning();
+            _parser.Received(3).ReadAsync().IgnoreAwaitWarning();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public async void ReadAsyncShouldReturnTrueIfThereAreRecordsInTheDatabase([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public async void ReadAsyncShouldReturnTrueIfThereAreRecordsInTheDatabase()
         {
             // Arrange
-            parser.ReadAsync().Returns(Task.FromResult(true));
+            _parser.ReadAsync().Returns(Task.FromResult(true));
 
             // Act
-            var result = await reader.ReadAsync();
+            var result = await _reader.ReadAsync();
 
             // Assert
             result.Should().BeTrue();
-            parser.Received().ReadAsync().IgnoreAwaitWarning();
+            _parser.Received().ReadAsync().IgnoreAwaitWarning();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void ReadShouldReturnFalseWhenThereAreNoMoreRecordsToRead([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void ReadShouldReturnFalseWhenThereAreNoMoreRecordsToRead()
         {
             // Arrange
-            parser.Read().Returns(true, true, false);
+            _parser.Read().Returns(true, true, false);
 
             // Act
-            var firstRead = reader.Read();
-            var secondRead = reader.Read();
-            var thirdRead = reader.Read();
+            var firstRead = _reader.Read();
+            var secondRead = _reader.Read();
+            var thirdRead = _reader.Read();
 
             // Assert
             firstRead.Should().BeTrue();
             secondRead.Should().BeTrue();
             thirdRead.Should().BeFalse();
-            parser.Received(3).Read();
+            _parser.Received(3).Read();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void ReadShouldReturnTrueIfThereAreRecordsInTheDatabase([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void ReadShouldReturnTrueIfThereAreRecordsInTheDatabase()
         {
             // Arrange
-            parser.Read().Returns(true);
+            _parser.Read().Returns(true);
 
             // Act
-            var result = reader.Read();
+            var result = _reader.Read();
 
             // Assert
             result.Should().BeTrue();
-            parser.Received().Read();
+            _parser.Received().Read();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void RecordsAffectedShouldBeZeroAfterReaderIsClosed([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void RecordsAffectedShouldBeZeroAfterreaderIsClosed()
         {
             // Act
-            reader.Close();
+            _reader.Close();
 
             // Assert
-            reader.RecordsAffected.Should().Be(0);
+            _reader.RecordsAffected.Should().Be(0);
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void RecordsAffectedShouldReturnRecordCountAfterRead([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void RecordsAffectedShouldReturnRecordCountAfterRead()
         {
             // Arrange
-            parser.RecordsAffected.Returns(3);
+            _parser.RecordsAffected.Returns(3);
 
             // Act
-            reader.Read();
+            _reader.Read();
 
             // Assert
-            reader.RecordsAffected.Should().Be(3);
-            parser.Received().RecordsAffected.IgnoreUnusedVariable();
+            _reader.RecordsAffected.Should().Be(3);
+            _parser.Received().RecordsAffected.IgnoreUnusedVariable();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public async void RecordsAffectedShouldReturnRecordCountAfterReadAsync([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public async void RecordsAffectedShouldReturnRecordCountAfterReadAsync()
         {
             // Arrange
-            parser.RecordsAffected.Returns(3);
+            _parser.RecordsAffected.Returns(3);
 
             // Act
-            await reader.ReadAsync();
+            await _reader.ReadAsync();
 
             // Assert
-            reader.RecordsAffected.Should().Be(3);
-            parser.Received().RecordsAffected.IgnoreUnusedVariable();
+            _reader.RecordsAffected.Should().Be(3);
+            _parser.Received().RecordsAffected.IgnoreUnusedVariable();
         }
 
-        [Theory]
-        [AutoNSubstituteData]
-        public void RecordsAffectedShouldThrowInvalidOperationExceptionIfNoDataHasBeenRead([Frozen] IXbaseDataParser parser, XbaseDataReader reader)
+        [Fact]
+        public void RecordsAffectedShouldThrowInvalidOperationExceptionIfNoDataHasBeenRead()
         {
             // Act
-            Action recordsAffected = () => reader.RecordsAffected.IgnoreUnusedVariable();
+            Action recordsAffected = () => _reader.RecordsAffected.IgnoreUnusedVariable();
 
             // Assert
             recordsAffected
                 .ShouldThrow<InvalidOperationException>()
                 .WithMessage(_noDataHasBeenReadExceptionMessage);
-        }
-
-        [Fact]
-        public void DummyFactToForceNCrunchToRun()
-        {
-            // See http://forum.ncrunch.net/yaf_postst358_Doesnt-run--Theory--tests.aspx for details
         }
     }
 }
